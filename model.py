ef75a49128008de8e3c19b1a7a6b0d025b27a26c
glwhappen <1597721684@qq.com>
2023-11-07 10:51:43 +0800

    model添加错误处理

--- model.py
+++ model.py
@@ -1,29 +1,51 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import os
+import logging
 
 class CodeGenerator:
-    def __init__(self, model='Salesforce/codegen-2B-mono'):
-        print("加载模型")
-        base_path = "./models"
-        path = os.path.join(base_path, model)
+    def __init__(self, model=None):
+        # 配置日志
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
+        # 确定基础路径和使用的模型
+        base_path = os.getenv('MODEL_BASE_PATH', './models')
+        model_name = model if model is not None else os.getenv('MODEL_NAME', 'Salesforce/codegen-2B-mono')
+
+        self.logger.info(f"正在加载模型: {model_name}")
+
+        model_path = os.path.join(base_path, model_name)
         
-        # 检查CUDA是否可用并设置设备
+        # 检查CUDA是否可用，并设置设备
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("启动模式", self.device)
+        self.logger.info(f"设备设置为 {self.device}")
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model, cache_dir=os.path.join(path, 'config'), local_files_only=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model, cache_dir=os.path.join(path, 'models'), local_files_only=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=os.path.join(model_path, 'config'), local_files_only=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=os.path.join(model_path, 'models'), local_files_only=True)
 
-        # 将模型放到设备上
+        # 将模型移动到适当的设备上
         self.model.to(self.device)
 
+        # 确保模型处于评估模式
+        self.model.eval()
+
     def predict(self, text, max_length=5):
-        print('进行补全', text)
-        # 确保输入张量也在正确的设备上
-        input_ids = self.tokenizer(text, return_tensors="pt").input_ids.to(self.device)
-        generated_ids = self.model.generate(input_ids, max_length=input_ids.shape[1] + max_length)
-        generated_tokens = generated_ids[0][-max_length:]
-        
-        # 在返回之前将生成的张量转回CPU
-        return self.tokenizer.decode(generated_tokens.cpu(), skip_special_tokens=True)
+        self.logger.info(f"为以下文本生成补全: {text}")
+        try:
+            # 禁用梯度计算
+            with torch.no_grad():
+                input_ids = self.tokenizer(text, return_tensors="pt").input_ids.to(self.device)
+                generated_ids = self.model.generate(input_ids, max_length=input_ids.shape[1] + max_length)
+            generated_tokens = generated_ids[0][-max_length:]
+            return self.tokenizer.decode(generated_tokens.cpu(), skip_special_tokens=True)
+        except RuntimeError as e:
+            if "CUDA out of memory" in str(e):
+                self.logger.error("CUDA内存不足：您可能需要减少批量大小或序列长度。")
+                raise RuntimeError("CUDA内存不足：您可能需要减少批量大小或序列长度。") from e
+            else:
+                raise e
+
+# 使用示例
+# generator = CodeGenerator()
+# prediction = generator.predict("快速的棕色狐狸", max_length=50)
